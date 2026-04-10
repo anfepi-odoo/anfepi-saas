d39bfecb20fb8a63c1048b73f68e6715cdc18b53
Roberto Requejo <rrequejo@anfepi.com>
2026-04-10 00:51:32 -0600

    Add pre-migrate script to fix DIOT tax_tag_invert on upgrade to 19.0

---
+++ anfepi_account_fix/migrations/19.0.1.0.0/pre-migrate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    """
+    Fix DIOT tax_tag_invert antes de que _process_end del módulo account
+    (saas~18.5.1.4/pre-remove-tax-tag-invert.py) ejecute su check.
+
+    Este script corre durante el upgrade del módulo (cuando ya estaba instalado).
+    El post_init_hook cubre el caso de instalación nueva.
+    """
+    cr.execute("""
+        UPDATE account_move_line
+        SET tax_tag_invert = FALSE
+        WHERE tax_tag_invert = TRUE
+          AND EXISTS (
+            SELECT 1
+            FROM account_account_tag_account_move_line_rel r
+            JOIN account_account_tag aat ON aat.id = r.account_account_tag_id
+            WHERE r.account_move_line_id = account_move_line.id
+              AND aat.name::text LIKE '%DIOT%'
+          )
+    """)
+    _logger.info(
+        'anfepi_account_fix migration 19.0.1.0.0: '
+        'DIOT tax_tag_invert fix aplicado (%d filas)',
+        cr.rowcount,
+    )
